462b2985d3a9ff2364e21628ee8a344cfe6df3f6
Katty020 <mr.aryan122@gmail.com>
2024-12-21 10:30:35 +0530

    ok

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -5,11 +5,11 @@ import Header from './Header'
 
 export default function Layout() {
 	return (
-		<div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-row">
-			<Sidebar />
+		<div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-col md:flex-row">
+			<Sidebar className="w-full md:w-64 lg:w-80" />
 			<div className="flex flex-col flex-1">
-				<Header />
-				<div className="flex-1 p-4 min-h-0 overflow-auto">
+				<Header className="w-full" />
+				<div className="flex-1 p-2 sm:p-4 min-h-0 overflow-auto">
 					<Outlet />
 				</div>
 			</div>
